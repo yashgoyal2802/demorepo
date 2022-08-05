22a13b103a2704b0052d18eee286912eab5d7ce9
Yash Goyal <yashdineshgoyal@gmail.com>
2022-08-05 16:35:10 +0530

    new

---
+++ Incedo_Octavius_Demo_2/Data/Incedo_Octavius_Demo_2_kol_degree_map_table_Context.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Incedo_Octavius_Demo_2.Data
+{
+    public class Incedo_Octavius_Demo_2_kol_degree_map_table_Context : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public Incedo_Octavius_Demo_2_kol_degree_map_table_Context() : base("name=Incedo_Octavius_Demo_2_kol_degree_map_table_Context")
+        {
+        }
+
+        public System.Data.Entity.DbSet<Incedo_Octavius_Demo_2.Models.kol_degree_map_table> kol_degree_map_table { get; set; }
+    }
+}
---
+++ Incedo_Octavius_Demo_2/Models/KOL_DATA.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Incedo_Octavius_Demo_2.Models
+{
+    [Table("KOL_DATA")]
+    public class KOL_DATA
+    {
+        [Key]
+        public int kol_id { get; set; }
+        public int mdm_id { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string mail { get; set; }
+        public string address { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public int zip { get; set; }
+        public int profile_status { get; set; }
+    }
+}
